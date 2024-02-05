dd5bb0e2dbf7e9425f6476a788acf0e3d3be6209
maria <melikesultan027@gmail.com>
2024-02-05 13:35:09 +0300

    Initial Commit

---
+++ Datas/DatasApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatasApp.swift
+//  Datas
+//
+//  Created by Melike Sultan CAN on 5.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DatasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
